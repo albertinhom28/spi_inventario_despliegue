8b7916f2c86ca852ca743fd4316a039ae7a34e05
albertinhom28 <albertmassg28@gmail.com>
2026-06-26 12:41:10 -0400

    Preparar proyecto para despliegue en Railway

--- config.py
+++ config.py
@@ -3,9 +3,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+database_url = os.environ.get("DATABASE_URL")
+
+# Compatibilidad con Railway/Heroku
+if database_url and database_url.startswith("postgres://"):
+    database_url = database_url.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'clave-temporal'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "clave-temporal"
+    SQLALCHEMY_DATABASE_URI = database_url
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     WTF_CSRF_ENABLED = True
 
@@ -16,7 +26,7 @@ class ProductionConfig(Config):
     DEBUG = False
 
 config = {
-    'development': DevelopmentConfig,
-    'production': ProductionConfig,
-    'default': ProductionConfig
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "default": ProductionConfig
 }
