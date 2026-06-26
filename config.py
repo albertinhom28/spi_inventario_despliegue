6f89630e6c2404922f0354f4bc9b3a777e248d97
albertinhom28 <albertmassg28@gmail.com>
2026-06-26 11:02:57 -0400

    Preparar proyecto para despliegue en Railway

--- config.py
+++ config.py
@@ -18,5 +18,5 @@ class ProductionConfig(Config):
 config = {
     'development': DevelopmentConfig,
     'production': ProductionConfig,
-    'default': DevelopmentConfig
+    'default': ProductionConfig
 }
--- run.py
+++ run.py
@@ -1,6 +1,10 @@
+import os
 from app import create_app
 
-app = create_app('development')
+app = create_app(os.getenv("FLASK_ENV", "production"))
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
